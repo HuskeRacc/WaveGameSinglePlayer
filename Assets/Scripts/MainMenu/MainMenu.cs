79b26ad7421adb20551c9d912b839dd1d04a0c4e
HuskeRacc <Huskcsgoacc@gmail.com>
2022-11-21 03:28:39 +0000

    Main Menu Added

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public GameObject mainMenuGO;
+
+    public void Play()
+    {
+        // can add a level selection.
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        optionsPanel.SetActive(true);
+
+        mainMenuGO.SetActive(false);
+    }
+
+    public void OptionsBackButton()
+    {
+        optionsPanel.SetActive(false);
+
+        mainMenuGO.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
